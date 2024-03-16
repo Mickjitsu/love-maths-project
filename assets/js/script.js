d3952fba68f47b97bd3f69d3eda1849a3c7b8b9f
Michael McCourt <mmccourt928@outlook.com>
2024-03-16 09:03:35 +0000

    first event listener add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// wait for the DOM to finish loading before running the game
+// get button emelemts and add event lsiteners to them
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener('click', function(){
+            if (this.getAttribute("data-type" === "submit")){
+                alert("You cliked submit");}
+                else {
+                    let gameType=this.getAttribute("data-type");
+                    alert(`You clicked ${gameType}`);
+                }
+            })
+        }
+    })
+
 function runGame(){
 
 }
@@ -27,5 +45,5 @@ function displayMultiplyQuestion(){
 }
 
 function displaySubtractQuestion(){
-    
+
 }
