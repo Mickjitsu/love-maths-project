4f908ada11a497cf020f22e79efa91422d4351b1
Michael McCourt <mmccourt928@outlook.com>
2024-03-17 19:02:59 +0000

    start of division game added

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,6 +46,9 @@ function runGame(gameType){
     else if(gameType==='subtract'){
         displaySubtractQuestion(num1, num2);
     }
+    else if(gameType==='division'){
+        displayDivisionQuestion(num1, num2);
+    }
     else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -86,6 +89,9 @@ function calculateCorrectAnswer(){
     }else if(operator === '-'){
         return [operand1 - operand2, "subtract"];
     }
+    else if(operator === '/'){
+        return [operand1 / operand2, "division"];
+    }
     else {
         alert(`unimplemented operator ${operator}`);
         throw `unimplemented operator ${operator} aborting!`;
@@ -123,4 +129,10 @@ function displaySubtractQuestion(operand1, operand2){
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = '-';
+}
+
+function displayDivisionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '/';
 }
